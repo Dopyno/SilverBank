d2af8cd676bb3c24b02ee84b58041a44a817cc5d
Dopyno <dopyno@yahoo.com>
2025-03-05 19:25:10 +0000

    event

--- script.js
+++ script.js
@@ -145,6 +145,16 @@ btnScrollTo.addEventListener('click', function (e) {
 
 const h1 = document.querySelector('h1');
 
-h1.addEventListener('mouseenter', function (e) {
+const alertH1 = function (e) {
   alert('addEventListener: Great! You are reading the heading :D');
-});
+
+  // h1.removeEventListener('mouseenter', alertH1)
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 4000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// }
