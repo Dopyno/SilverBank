89a881c1ca742d478d78523c7118ed5493be5f39
Dopyno <dopyno@yahoo.com>
2025-03-13 18:39:47 +0000

    tabs

--- script.js
+++ script.js
@@ -89,17 +89,24 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 //?  ============  Tabbed component  =============
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operation__content');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
-  console.log(clicked);
 
   if (!clicked) return;
 
-  //Active tab
+  //Remove active tab
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //Activate tab
   clicked.classList.add('operations__tab--active');
+
+  //?   Active content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
 
 ///////////////////////////////////////
