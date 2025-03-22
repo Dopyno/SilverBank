47e64410ced15d2077d693c0c433eeeb6c2b9bc0
Dopyno <dopyno@yahoo.com>
2025-03-22 12:49:31 +0000

    lazy loading element

--- script.js
+++ script.js
@@ -191,6 +191,35 @@ allSection.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+//! ===============================================================
+//?   ========== Lazy loading images ==============================
+
+const imgTarget = document.querySelectorAll('img[data-src]'); //*select img only with a property data-src
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  //* replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target)
+};
+
+const imgOption = {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px',
+};
+
+const imgObserver = new IntersectionObserver(loadImg, imgOption);
+
+imgTarget.forEach(img => imgObserver.observe(img));
 ///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////
