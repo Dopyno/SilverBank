0c474320e94783a64cd980c8c68d7e24dc981a50
Dopyno <dopyno@yahoo.com>
2025-03-23 08:42:42 +0000

    slider

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ const loadImg = function (entries, observer) {
   entry.target.addEventListener('load', function () {
     entry.target.classList.remove('lazy-img');
   });
-  observer.unobserve(entry.target)
+  observer.unobserve(entry.target);
 };
 
 const imgOption = {
@@ -221,8 +221,44 @@ const imgObserver = new IntersectionObserver(loadImg, imgOption);
 imgTarget.forEach(img => imgObserver.observe(img));
 
 //! ===============================================================
-//?  ===========  
+//?  ===========  Slider ==================================
 
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+//next side
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
 
 ///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
